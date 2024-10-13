4e1e36123f16042e07e901dc9a83a6025f27bccb
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-10-13 15:48:27 +0800

    新增首頁區塊動畫

--- src/components/page/home/Preserve.tsx
+++ src/components/page/home/Preserve.tsx
@@ -14,19 +14,38 @@ export default function Preserve() {
         title="線上預約"
         subTitle="即時瞭解您的需求，安排最適合毛孩的寵物保姆！"
       />
-      <div className="grid grid-cols-1 lg:grid-cols-4  gap-[6px]">
-        <div className="   bg-black-soft relative">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-[6px]">
+        <div className="relative flex justify-center items-center">
           <Image src={img1} alt="" fill className="object-cover" />
+          <span className="text-2xl font-bold mx-auto text-white relative">
+            寵物寄宿
+          </span>
         </div>
-        <div className="  relative">
+        <div className="relative flex justify-center items-center">
           <Image src={img2} alt="" fill className="object-cover" />
+          <span className="text-2xl font-bold mx-auto text-white relative">
+            寵物美容
+          </span>
         </div>
-        <div className="   bg-black-soft flex flex-col relative gap-[6px]">
-          <Image src={img3} alt="" className="object-cover" />
-          <Image src={img4} alt="" className="object-cover" />
+        <div className="flex flex-col h-[550px] relative gap-[6px]">
+          <figure className="flex-1    relative flex justify-center items-center">
+            <Image src={img3} alt="" className="object-cover" fill />
+            <span className="text-2xl font-bold mx-auto text-white relative ">
+              到府照顧
+            </span>
+          </figure>
+          <figure className="flex-1   relative flex justify-center items-center">
+            <Image src={img4} alt="" className="object-cover" fill />
+            <span className="text-2xl font-bold mx-auto text-white  relative">
+              遛狗保姆
+            </span>
+          </figure>
         </div>
-        <div className="relative ">
+        <div className="relative flex justify-center items-center">
           <Image src={img5} alt="" fill className="object-cover" />
+          <span className="text-2xl font-bold mx-auto text-white relative">
+            行為訓練
+          </span>
         </div>
       </div>
     </div>
--- src/components/page/home/ServiceBanner.tsx
+++ src/components/page/home/ServiceBanner.tsx
@@ -1,10 +1,47 @@
+"use client";
 import React from "react";
 import img3 from "@/../public/index-9.jpeg";
 import Image from "next/image";
+import CountUp from "react-countup";
+interface SectionTitleType {
+  title: number;
+  subTitle: string;
+}
 export default function ServiceBanner() {
+  const tilles: SectionTitleType[] = [
+    {
+      title: 5278,
+      subTitle: "人使用 PETPET",
+    },
+    {
+      title: 580,
+      subTitle: "人成為寵物保母",
+    },
+    {
+      title: 10000,
+      subTitle: "已完成服務",
+    },
+  ];
   return (
-    <div>
-      <Image src={img3} alt="" className="w-full max-h-[584px] object-cover" />
+    <div className="relative">
+      <Image src={img3} alt="" className="w-full max-h-[710px] object-cover " />
+      <div className="absolute top-20 left-32 text-white">
+        <div className="lg:text-[40px] ">PETPET目前已有</div>
+        <div className="mt-[100px] flex  gap-[30px]">
+          {tilles.map((item, idx) => (
+            <div key={idx} className="flex gap-5">
+              <div className="h-80 w-1 bg-white" />
+              <div>
+                {/* <div className=" text-5xl leading-[72px]">{item.title}</div> */}
+                <div className="text-5xl leading-[72px]">
+                  <CountUp end={item.title} duration={2} separator="," />
+                </div>
+                <div className="text-xl">{item.subTitle}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
